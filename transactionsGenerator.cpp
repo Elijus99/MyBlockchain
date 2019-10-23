4f4f462f976bf1a441b4972895b0314824bf5518
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-10-23 16:02:42 +0300

    v0.2 - validating transactions

--- transactionsGenerator.cpp
+++ transactionsGenerator.cpp
@@ -1,10 +1,10 @@
 #include "transactionsGenerator.h"
 
-Transaction GenerateTransaction(size_t MaxUserId, double minTran, double maxTran)
+Transaction GenerateTransaction(vector<user> users, double minTran, double maxTran)
 {
     std::random_device rd;
     std::mt19937 mt(rd());
-    std::uniform_int_distribution <int> genInt(0, (MaxUserId - 1));
+    std::uniform_int_distribution <int> genInt(0, (users.size() - 1));
     std::uniform_real_distribution <double> genDouble(minTran, maxTran);
     Transaction temp;
     do {
@@ -12,23 +12,24 @@ Transaction GenerateTransaction(size_t MaxUserId, double minTran, double maxTran
         temp.receiver_id = std::to_string(genInt(mt));
     } while (temp.sender_id == temp.receiver_id);
     temp.value = genDouble(mt);
+    temp.transaction_id = hash(users[std::stoi(temp.sender_id)].public_key + " sent " + users[std::stoi(temp.receiver_id)].public_key + " " + std::to_string(temp.value));
     return temp;
 }
-vector<Transaction> GenerateTransactionsPool(size_t MaxUserId, size_t count, double minTran, double maxTran)
+vector<Transaction> GenerateTransactionsPool(vector<user> users, size_t count, double minTran, double maxTran)
 {
     vector<Transaction> TransactionsPool;
     for (size_t i = 0; i < count; i++) {
-        TransactionsPool.push_back(GenerateTransaction(MaxUserId, minTran, maxTran));
+        TransactionsPool.push_back(GenerateTransaction(users, minTran, maxTran));
     }
     return TransactionsPool;
 }
-void GenerateTransactionsPoolFile(const string &fileName, size_t MaxUserId, size_t count, double minTran, double maxTran)
+void GenerateTransactionsPoolFile(const string &fileName, vector<user> users, size_t count, double minTran, double maxTran)
 {
     std::ofstream out(fileName);
     for (size_t i = 0; i < count; i++) {
         Transaction temp;
-        temp = GenerateTransaction(MaxUserId, minTran, maxTran);
-        out << temp.sender_id << " " << temp.receiver_id << " " << temp.value << std::endl;
+        temp = GenerateTransaction(users, minTran, maxTran);
+        out << temp.sender_id << " " << temp.receiver_id << " " << std::to_string(temp.value) << " " << temp.transaction_id << std::endl;
     }
 }
 vector<Transaction> ReadTransactionsPoolFile(const string& fileName)
@@ -37,7 +38,7 @@ vector<Transaction> ReadTransactionsPoolFile(const string& fileName)
     Transaction temp;
     std::ifstream in(fileName);
     while (!in.eof()) {
-        in >> temp.sender_id >> temp.receiver_id >> temp.value;
+        in >> temp.sender_id >> temp.receiver_id >> temp.value >> temp.transaction_id;
         transactions.push_back(temp);
     }
     return transactions;
@@ -54,9 +55,16 @@ vector<Transaction> SelectTransactions(vector<Transaction>& TransactionsPool, ve
         temp.sender_id = users[std::stoi(TransactionsPool[i].sender_id)].public_key;
         temp.receiver_id = users[std::stoi(TransactionsPool[i].receiver_id)].public_key;
         temp.value = TransactionsPool[i].value;
-        users[std::stoi(TransactionsPool[i].sender_id)].ECoin -= temp.value;
-        users[std::stoi(TransactionsPool[i].receiver_id)].ECoin += temp.value;
-        BlockTransactions.push_back(temp);
+        if (users[std::stoi(TransactionsPool[i].sender_id)].ECoin > temp.value) {
+            if (TransactionsPool[i].transaction_id == hash(temp.sender_id + " sent " + temp.receiver_id + " " + std::to_string(temp.value))) {
+                users[std::stoi(TransactionsPool[i].sender_id)].ECoin -= temp.value;
+                users[std::stoi(TransactionsPool[i].receiver_id)].ECoin += temp.value;
+                BlockTransactions.push_back(temp);
+            }
+            else {
+                cout << "A forged transaction was found" << endl;
+            }
+        }
         TransactionsPool.erase(TransactionsPool.begin() + i);
     }
     return BlockTransactions;
--- transactionsGenerator.h
+++ transactionsGenerator.h
@@ -2,6 +2,7 @@
 #ifndef TRANSACTIONS_GENERATOR_H_INCLUDED
 #define TRANSACTIONS_GENERATOR_H_INCLUDED
 #include "usersGenerator.h"
+#include "hash.h"
 #include <fstream>
 #include <string>
 #include <random>
@@ -12,11 +13,12 @@ struct Transaction {
     string sender_id;
     string receiver_id;
     double value;
+    string transaction_id;
 };
 
-Transaction GenerateTransaction(size_t MaxUserId, double minTran, double maxTran);
-vector<Transaction> GenerateTransactionsPool(size_t MaxUserId, size_t count, double minTran, double maxTran);
-void GenerateTransactionsPoolFile(const string &fileName, size_t MaxUserId, size_t count, double minTran, double maxTran);
+Transaction GenerateTransaction(vector<user> users, double minTran, double maxTran);
+vector<Transaction> GenerateTransactionsPool(vector<user> users, size_t count, double minTran, double maxTran);
+void GenerateTransactionsPoolFile(const string &fileName, vector<user> users, size_t count, double minTran, double maxTran);
 vector<Transaction> ReadTransactionsPoolFile(const string& fileName);
 vector<Transaction> SelectTransactions(vector<Transaction>& TransactionsPool, vector<user>& users);
 #endif
